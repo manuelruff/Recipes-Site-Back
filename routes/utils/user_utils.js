81ec2d113efe97c633b7d5807f04904351c2d301
Manuel Ruffieux <manuel42994299@gmail.com>
2024-07-03 16:29:55 +0300

    fixed get meals

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,4 +1,6 @@
 const DButils = require("./DButils");
+const recipes_utils = require("./recipes_utils");
+
 
 async function markAsFavorite(user_id, recipe_id){
     await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
@@ -13,10 +15,18 @@ async function removeFavorite(user_id, recipe_id) {
     await DButils.execQuery(`DELETE FROM FavoriteRecipes WHERE user_id='${user_id}' AND recipe_id=${recipe_id}`);
   }
 
-async function getMeals(user_id){
-    const meals = await DButils.execQuery(`select recipe_id from usermeals where user_id='${user_id}'`);
-    return meals;
+async function getMeals(user_id) {
+  const mealIds = await DButils.execQuery(`SELECT recipe_id FROM usermeals WHERE user_id='${user_id}'`);
+  const mealDetailsPromises = mealIds.map(meal => recipes_utils.getRecipeDetails(meal.recipe_id));
+  try {
+      const mealDetails = await Promise.all(mealDetailsPromises);
+      return mealDetails;
+  } catch (error) {
+      console.error("Failed to fetch meals:", error);
+      throw error;
+  }
 }
+
 async function markAsMeal(user_id, recipe_id){
   await DButils.execQuery(`insert into usermeals values ('${user_id}',${recipe_id})`);
 }
